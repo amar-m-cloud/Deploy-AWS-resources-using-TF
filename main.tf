d387a999e5f8f4a3be6b88213cdf156e249f2e44
NAZIYASHAIK1695 <101784632+NAZIYASHAIK1695@users.noreply.github.com>
2022-09-01 15:54:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIA2BV2M3J3QNNQTRCA"
-  secret_key = "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"
+  access_key = "AK"
+  secret_key = "SR"
 }
